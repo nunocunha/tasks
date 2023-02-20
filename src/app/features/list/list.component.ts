6a31bd5da1d99f1d5ebefa04f151b06ee6816adf
Nuno Cunha <me@nunocunha.pt>
2023-02-20 23:38:13 +0000

    Create list feature skeleton

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'tasks-list',
+    templateUrl: './list.component.html',
+    styleUrls: [
+        '../../components/base.css',
+        './list.component.css',
+    ],
+})
+export class ListComponent {
+
+}
