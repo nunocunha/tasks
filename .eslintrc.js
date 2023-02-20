2a3dc5c6c7a1ea973813671d52d3ebad67e21954
Nuno Cunha <me@nunocunha.pt>
2023-02-20 22:37:49 +0000

    Increase strictness on TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,12 +14,38 @@ module.exports = {
         },
         {
             files: ['*.ts'],
+            parserOptions: {
+                project: ['tsconfig.json'],
+            },
             extends: [
                 'eslint:recommended',
                 'plugin:@typescript-eslint/recommended',
                 'plugin:@angular-eslint/recommended',
                 'plugin:@angular-eslint/template/process-inline-templates',
             ],
+            rules: {
+                '@typescript-eslint/explicit-member-accessibility': 'error',
+                '@typescript-eslint/explicit-function-return-type': [
+                    'error',
+                    {
+                        allowExpressions: true,
+                    },
+                ],
+                '@typescript-eslint/strict-boolean-expressions': [
+                    'error',
+                    {
+                        allowString: false,
+                        allowNumber: false,
+                        allowNullableObject: false,
+                        allowNullableBoolean: false,
+                        allowNullableString: false,
+                        allowNullableNumber: false,
+                        allowNullableEnum: false,
+                        allowAny: false,
+                        allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: false,
+                    },
+                ],
+            },
         },
         {
             files: ['cypress/support/**/*.ts'],
