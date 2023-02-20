43bce6f796369ab971f95a3937503b680a01a2cf
Nuno Cunha <me@nunocunha.pt>
2023-02-20 23:39:27 +0000

    Add app structure with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component, HostBinding} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {AppModule} from './app.module';
 
 @Component({
     selector: 'tasks-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    public mobileQuery: MediaQueryList;
+    public routes = AppModule.mainRoutes;
+    @HostBinding('class.container') private hasContainerClass = true;
+    @HostBinding('class.is-mobile') private hasMobileClass = false;
+
+    public constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia('(max-width: 600px)');
+        this.mobileQuery.addEventListener('change', () => {
+            this.hasMobileClass = this.mobileQuery.matches;
+            changeDetectorRef.detectChanges();
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {RouterModule, Routes} from '@angular/router';
+import {MenuModule} from './components/menu/menu.module';
+import {ListComponent} from './features/list/list.component';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -11,9 +20,40 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        RouterModule.forRoot(AppModule.rootRoutes),
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MenuModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {
+    public static readonly mainRoutes: Routes = [
+        {
+            path: 'list',
+            component: ListComponent,
+            data: {
+                name: 'List',
+                icon: 'checklist',
+            },
+        },
+    ];
+
+    public static readonly rootRoutes: Routes = [
+        {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'list',
+        },
+        ...AppModule.mainRoutes,
+        {
+            path: '**',
+            component: class Error {
+            },
+        },
+    ];
 }
