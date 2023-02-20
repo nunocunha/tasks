d416aa6117c9091fa0a61110e242802a3c7ec0f8
Nuno Cunha <me@nunocunha.pt>
2023-02-20 22:34:23 +0000

    Force minimum code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,22 @@
 import type {Config} from 'jest';
 
+const COVERAGE_THRESHOLD: Readonly<number> = 80;
+
 const jestConfig: Config = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
     globalSetup: 'jest-preset-angular/global-setup',
     testMatch: ['<rootDir>/src/app/**/*.spec.ts'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            branches: COVERAGE_THRESHOLD,
+            functions: COVERAGE_THRESHOLD,
+            lines: COVERAGE_THRESHOLD,
+            statements: COVERAGE_THRESHOLD,
+        }
+    }
 };
 
 export default jestConfig;
